df0fa934d9c1ce16b81fbec7229040a1a025f69c
Tue Vo <voth0@sewanee.edu>
2016-08-16 16:25:16 -0400

    Create MiniParser.java

---
+++ MiniParser.java
@@ -0,0 +1,123 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class MiniParser {
+
+    public NestedInteger deserialize(String s) {
+
+ 		Stack<Character> st = new Stack<Character>();
+ 		NestedInteger result = new NestedInteger();
+ 		String num="";
+ 		
+ 		if (s.equals("")) return result;
+
+ 		else if (s.charAt(0)=='[' && s.charAt(s.length()-1)==']') 
+ 			return deserialize(s.substring(1, s.length()-1));
+
+ 		for (int i=0; i<=s.length(); i++) {
+ 			if (i < s.length() && s.charAt(i)=='[') {
+ 				int mark=i;
+ 				st.push('[');
+ 				while (!st.empty() || i<s.length()) {
+ 					i++;
+ 					if (s.charAt(i)=='[') st.push('[');
+ 					else if (s.charAt(i)==']') st.pop();
+ 				}
+ 				result.add(deserialize(s.substring(mark, i)));
+ 			}
+
+ 			else if ((i < s.length() && (s.charAt(i) == '-' || s.charAt(i) == ',')) 
+ 					|| (i == s.length() && num.length() > 0)) {
+ 				result.setInteger(Integer.parseInt(num));
+ 				num="";
+ 			}
+
+ 			else {
+ 				num+=s.charAt(i);
+ 			}
+ 		}       
+
+ 		return result;
+    }
+}
